3abe881249268c5e71492185222b568192826e0b
Adrian Sanchez <131232601+adrianjr18@users.noreply.github.com>
2023-05-25 23:50:35 -0700

    Update 07-while-loop-translation.js

--- 07-while-loop-translation.js
+++ 07-while-loop-translation.js
@@ -6,21 +6,21 @@ to use a `while` loop instead of a `for` loop! If you forget the syntax for a
 `while` loop go back and check out the reading.
 */
 
-
 function eCounter(word) {
-    let count = 0;
+  let count = 0;
+  let index = 0;
 
-    // convert this for loop into a while loop
-    for (let index = 0; index < word.length; index++) {
-        let char = word[index];
-        if (char === "e" || char === "E") {
-            count += 1;
-        }
+  while (index < word.length) {
+    let char = word[index];
+    if (char === "e" || char === "E") {
+      count += 1;
     }
+    index += 1;
+  }
 
-    return count;
-};
+  return count;
+}
 
-console.log(eCounter("apple")); //  1
-console.log(eCounter("appleapple")); //  2
-console.log(eCounter("Appleee")); //  3
+console.log(eCounter("apple")); // => 1
+console.log(eCounter("appleapple")); // => 2
+console.log(eCounter("Appleee")); // => 3
