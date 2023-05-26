bf6038b928f5c24a0e14ce46d507baee732fe995
Adrian Sanchez <131232601+adrianjr18@users.noreply.github.com>
2023-05-25 23:46:30 -0700

    Update 03-min-value.js

--- 03-min-value.js
+++ 03-min-value.js
@@ -6,6 +6,18 @@ If the array is empty, the function should return `null`.
 */
 
 // Your code here
+// your code here
+function minValue(nums)	{
+let min=null;
+for(let i=0;i<nums.length;i++){ 
+let number =nums[i]
+if(min === null ||number <min){
+min =number;
+}
+}
+return min;
+}
+
 
 
 console.log(minValue([4, 6, 3, 5, 2, 4])); // 2
